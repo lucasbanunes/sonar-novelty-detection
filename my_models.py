6788c7932ecb8b2dab6be878468384ba5eddb54d
lucasbanunes <lucasbanunes@gmail.com>
2020-08-07 16:35:45 -0300

    Changed my_models.py according to the MultiInitLog implementation.

--- my_models.py
+++ my_models.py
@@ -1,126 +1,169 @@
 import os
 from copy import deepcopy
+
 import numpy as np
-import tensorflow as tf
-import tensorflow.keras as keras
+from tensorflow import keras
 from tensorflow.keras import callbacks
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPool2D
-from data_analysis.neural_networks import models, losses, metrics
-import sonar_utils
+from data_analysis.neural_networks import losses, metrics, training
+from data_analysis.utils.utils import to_sparse_tanh, DataSequence, shuffle_pair
+from data_analysis.utils.lofar_operators import LofarImgSequence
+
+NUMBER_OF_INITS = 10
 
 class standard_model():
     is_expert_committee = False
 
     @staticmethod
-    def compile_and_fit(model,train_set, val_set, cache, bot=None, chat_id=None):
-        model.compile(loss=keras.losses.mean_squared_error, 
-                    optimizer = keras.optimizers.Adam( learning_rate=0.001),
-                    metrics = [metrics.sparse_accuracy])
+    def compile_and_fit(model,train_set, val_set, bot=None, chat_id=None, message_id=None):
+
+        optimizer = keras.optimizers.Adam(learning_rate=0.001)
         early_stop = callbacks.EarlyStopping(monitor = 'val_sparse_accuracy', min_delta=0.001, patience=30, restore_best_weights=True, mode='max')
-        lr = callbacks.ReduceLROnPlateau(monitor='val_sparse_accuracy', factor=0.1, patience=7, min_lr=0.000001, mode='max')
-        cb = [early_stop, lr]
-        if bot is None:
-            log = model.multi_init_fit(x=train_set, epochs=1000, n_inits=10, init_metric='val_sparse_accuracy', 
-                    validation_data=val_set, save_inits=True, cache_dir=cache, callbacks=cb)
-        else:
-            log = model.multi_init_fit(x=train_set, epochs=1000, n_inits=10, init_metric='val_sparse_accuracy', 
-                                validation_data=val_set, save_inits=True, cache_dir=cache, callbacks=cb, 
-                                inits_functions=[sonar_utils.SendInitMessage(bot, chat_id)])
+        lr_reducer = callbacks.ReduceLROnPlateau(monitor='val_sparse_accuracy', factor=0.1, patience=7, min_lr=0.000001, mode='max')
+        multi_init = training.MultiInitLog()
+
+        for init in range(NUMBER_OF_INITS):
+            if not bot is None:
+                bot.sendMessage(chat_id, message_id + f'Starting init {init+1}')
+
+            model.compile(loss=keras.losses.mean_squared_error, 
+                        optimizer = deepcopy(optimizer),
+                        metrics = [metrics.sparse_accuracy])
+            cb = [deepcopy(early_stop), deepcopy(lr_reducer)]
+
+            log = model.fit(x=train_set, epochs=1000, validation_data=val_set, callbacks=cb)
+
+            multi_init.add_initialization(model, log)
+
+            training.reinitialize_weights(model)
         
-        return log
+        return multi_init
 
 class expert_commitee():
-    is_expert_committee = True
     
     @staticmethod
     def get_experts(input_shape, intermediate_neurons, classes):
         raise NotImplementedError
 
     @staticmethod
-    def change_to_expert_data(classes, class_mapping, dataset):
-        expert_sets = dict()
-        for class_ in classes:
-            target_label = class_mapping[class_]
-            change_func = lambda x: np.array([1 if np.all(label == target_label) else -1 for label in x])
-            exp_set = deepcopy(dataset)
-            exp_set.apply(lambda x,y: (x, change_func(y)))
-            expert_sets[class_] = exp_set
-        return expert_sets
-
-    def get_committee(self, input_shape, classes, neurons, cache_dir):
-        return models.ExpertsCommittee(classes, self.get_experts(input_shape, neurons, classes), cache_dir)
+    def get_wrapper_layers(intermediate_neurons):
+        layers = [Dense(intermediate_neurons, activation='tanh'), Dense(3, activation='tanh')]
+        return layers
 
     @staticmethod
-    def get_wrapper(input_shape):
-        model = models.MultiInitSequential()
-        model.add(Dense(40, activation='tanh', input_shape=input_shape))
-        model.add(Dense(3, activation='tanh'))
-        return model
-
-    @staticmethod
-    def fit_wrapper(model, train_set, val_set, cache, bot=None, chat_id=None):
-        model.compile(loss=keras.losses.mean_squared_error, 
-            optimizer = keras.optimizers.Adam( learning_rate=0.01),
-            metrics = [metrics.sparse_accuracy])
-        early_stop = keras.callbacks.EarlyStopping(monitor = 'val_sparse_accuracy', min_delta=0.001, patience=30, mode='max')
-        lr = callbacks.ReduceLROnPlateau(monitor='val_sparse_accuracy', factor=0.1, patience=7, min_lr = 0.000001, mode='max')
-        if bot is None:
-            log = model.multi_init_fit(x=train_set, epochs=1000, n_inits=10, init_metric='val_sparse_accuracy', validation_data=val_set, save_inits=True,
-                        cache_dir=cache, callbacks=[early_stop, lr])
-        else:
-            log = model.multi_init_fit(x=train_set, epochs=1000, n_inits=10, init_metric='val_sparse_accuracy', validation_data=val_set, save_inits=True,
-                            cache_dir=cache, callbacks=[early_stop, lr], 
-                            inits_functions=[sonar_utils.SendInitMessage(bot, chat_id)])
+    def get_wrapped_committee(committee, wrapper_layers):
+        input_layer = keras.Input(tuple(committee.input.shape[1:]))
+        wrapper_experts = list()
+        for expert in committee.layers:
+            if training.is_model(expert):
+                expert_as_layer = keras.Model(expert.input, expert.layers[-2].output, name=expert.name)(input_layer)
+                wrapper_experts.append(expert_as_layer)
+        x = keras.layers.concatenate(wrapper_experts)
+        for layer in wrapper_layers:
+            x = layer(x)
+        wrapped_committee = keras.Model(input_layer, x, name='wrapped_committee')
         
-        return log
+        for expert in wrapped_committee.layers:
+            if training.is_model(expert):
+                expert.trainable = False
+
+        return wrapped_committee
 
     @staticmethod
-    def fit_committee(committee, train_set, val_set, bot=None, chat_id=None):
-        #Compile
-        classes = list(committee.experts.keys())
-        compile_mapping = dict(loss=losses.expert_mse, optimizer=keras.optimizers.Adam( learning_rate=0.01), metrics=[metrics.expert_accuracy])
-        compile_params = {class_: compile_mapping for class_ in classes}
-        committee.compile(compile_params)
+    def fit_wrapped_committee(model, train_set, val_set, bot=None, chat_id=None, message_id=None):
+        return standard_model.compile_and_fit(model,train_set, val_set, bot, chat_id, message_id)
 
-        #Fit
-        class_weights = {class_: None for class_ in classes}
-        init_metrics = {class_: 'val_expert_accuracy' for class_ in classes}
-        modes = {class_: 'max' for class_ in classes}
+    @staticmethod
+    def fit_committee(experts, classes, class_mapping, train_set, val_set, bot=None, chat_id=None, message_id=None):
+
+        input_layer = keras.Input(train_set.input_shape())
+        optimizer = keras.optimizers.Adam(learning_rate=0.01)
         early_stop = keras.callbacks.EarlyStopping(monitor = 'val_expert_accuracy', min_delta=0.001, patience=30, mode='max')
-        lr = callbacks.ReduceLROnPlateau(monitor='val_expert_accuracy', factor=0.1, patience=7, min_lr = 0.000001, mode='max')
-        if bot is None:
-            model, log = committee.fit(x=train_set, epochs=1000, validation_data=val_set, class_weight=class_weights, init_metric=init_metrics, 
-                            mode=modes, n_inits=10, save_inits=True, callbacks=[early_stop, lr])
-        else:
-            model, log = committee.fit(x=train_set, epochs=1000, validation_data=val_set, class_weight=class_weights, init_metric=init_metrics, 
-                                        mode=modes, n_inits=10, save_inits=True, callbacks=[early_stop, lr], 
-                                        inits_functions=[sonar_utils.SendInitMessage(bot, chat_id)])
-        
-        return model, log
+        lr = keras.callbacks.ReduceLROnPlateau(monitor='val_expert_accuracy', factor=0.1, patience=7, min_lr = 0.000001, mode='max')
+        callbacks = [early_stop, lr]
+        multi_inits = dict()
+        committee_experts = list()
+
+        for class_name in classes:
+
+            target_label = class_mapping[class_name]
+            change_func = lambda x: np.array([1 if np.all(label == target_label) else -1 for label in x])
+            set_func = lambda x,y: (x, change_func(y))
+            train_exp = deepcopy(train_set)
+            train_exp.apply(set_func)
+            val_exp = deepcopy(val_set)
+            val_exp.apply(set_func)
+
+            multi_init = training.MultiInitLog()
+            expert = experts[class_name]
+
+            for init in range(NUMBER_OF_INITS):
+                message = f'expert for class {class_name}\nStarting init {init+1}'
+                print(message)
+                if bot:
+                    bot.sendMessage(chat_id, message_id + message)
+
+                expert.compile(loss=losses.expert_mse, 
+                                optimizer=deepcopy(optimizer), 
+                                metrics=[metrics.expert_accuracy])
+
+                log = expert.fit(x=train_exp, epochs=1000, validation_data=val_exp, callbacks=deepcopy(callbacks))
+                multi_init.add_initialization(expert, log)
+                training.reinitialize_weights(expert)
+
+            expert.set_weights(multi_init.best_weights(metric = 'val_expert_accuracy', mode='max', training_end = False))
+            committee_experts.append(expert(input_layer))
+            multi_inits[class_name] = multi_init
+
+        concat_layer = keras.layers.concatenate(committee_experts)
+
+        committee = keras.Model(input_layer, concat_layer)
+
+        return committee, multi_inits
+
 
 class cnn(standard_model):
-    """class with the parameters I'm using as cnn"""
-    is_cnn = True
-    is_mlp = False
 
     @staticmethod
     def get_model(input_shape, conv_neurons):
-        model = models.MultiInitSequential()
-        model.add(keras.Input(input_shape))
-        model.add(Conv2D(conv_neurons, kernel_size=4, activation = 'tanh'))
+        model = keras.Sequential()
+        model.add(Conv2D(conv_neurons, kernel_size=4, activation = 'tanh', input_shape=input_shape))
         model.add(MaxPool2D())
         model.add(Flatten())
         model.add(Dense(3, activation='tanh'))
+
         return model
+    
+    @staticmethod
+    def get_data(base_path, split, to_known_value, args):
+        splitpath = os.path.join(base_path, f'kfold_{args.folds}_folds', f'window_size_{args.window_size}_stride_{args.stride}', f'split_{split}.npz')
+        split_set = np.load(splitpath, allow_pickle=True)
+
+        x_train, y_train = shuffle_pair(split_set['x_train'], split_set['y_train'])
+        x_val, y_val = shuffle_pair(split_set['x_val'], split_set['y_val'])
+        x_test, y_test = shuffle_pair(split_set['x_test'], split_set['y_test'])
+
+        train_set = LofarImgSequence(split_set['data'], 
+                                    x_train, 
+                                    to_sparse_tanh(to_known_value(y_train)))
+        val_set = LofarImgSequence(split_set['data'], 
+                                    x_val, 
+                                    to_sparse_tanh(to_known_value(y_val)))
+
+        x_test = np.concatenate((x_test, split_set['x_novelty']), axis=0)
+        y_test = np.concatenate((y_test, split_set['y_novelty']), axis=0)
+        test_set = LofarImgSequence(split_set['data'], x_test, y_test)
+
+        return train_set, val_set, test_set
 
 class cnn_expert(expert_commitee):
 
-    is_cnn = True
-    is_mlp = False
-
+    @staticmethod
+    def get_data(base_path, split, to_known_value, args):
+        return cnn.get_data(base_path, split, to_known_value, args)
     @staticmethod
     def get_experts(input_shape, conv_neurons, classes):
-        experts =list()
+        experts = dict()
         for class_name  in classes:
             expert_name = f'{class_name}_expert'
             input_layer = keras.Input(shape=input_shape, name=expert_name + '_input')
@@ -129,13 +172,10 @@ class cnn_expert(expert_commitee):
             x = Flatten()(x)
             output_layer = Dense(1, activation='tanh')(x) 
             model = keras.Model(input_layer, output_layer, name=expert_name)
-            experts.append(model)
-        return np.array(experts)
+            experts[class_name] = model
+        return experts
 
 class cnn_mlp_expert(expert_commitee):
-
-    is_cnn = True
-    is_mlp = False
 
     @staticmethod
     def get_experts(input_shape, conv_neurons, classes):
@@ -154,30 +194,47 @@ class cnn_mlp_expert(expert_commitee):
 
 
 class mlp(standard_model):
-    """class with the parameters I'm using as mlp"""
-    is_cnn = False
-    is_mlp = True
     
     @staticmethod
     def get_model(input_shape, intermediate_neurons):
-        model = models.MultiInitSequential()
-        model.add(Dense(intermediate_neurons, activation='tanh', input_shape=input_shape))
-        model.add(Dense(3, activation='tanh'))
+        input_layer = keras.Input(input_shape)
+        x = Dense(intermediate_neurons, activation='tanh', input_shape=input_shape)(input_layer)
+        output_layer = Dense(3, activation='tanh')(x)
+        model = keras.Model(input_layer, output_layer, name='mlp')
         return model
+
+    @staticmethod
+    def get_data(base_path, split, to_known_value, args):
+        splitpath = os.path.join(base_path, f'kfold_{args.folds}_folds', 'vectors', f'split_{split}.npz')
+        split_set = np.load(splitpath, allow_pickle=True)
+
+        x_train, y_train = shuffle_pair(split_set['x_train'], split_set['y_train'])
+        x_val, y_val = shuffle_pair(split_set['x_val'], split_set['y_val'])
+        x_test, y_test = shuffle_pair(split_set['x_test'], split_set['y_test'])
+        
+        train_set = DataSequence(x_train, to_sparse_tanh(to_known_value(y_train)))
+        val_set = DataSequence(x_val, to_sparse_tanh(to_known_value(y_val)))
+
+        x_test = np.concatenate((x_test, split_set['x_novelty']), axis=0)
+        y_test = np.concatenate((y_test, split_set['y_novelty']), axis=0)
+        test_set = DataSequence(x_test, y_test)
+
+        return train_set, val_set, test_set
 
 class mlp_expert(expert_commitee):
 
-    is_cnn = False
-    is_mlp = True
+    @staticmethod
+    def get_data(base_path, split, to_known_value, args):
+        return mlp.get_data(base_path, split, to_known_value, args)
 
     @staticmethod
     def get_experts(input_shape, intermediate_neurons, classes):
-        experts =list()
+        experts = dict()
         for class_name  in classes:
             expert_name = f'{class_name}_expert'
             input_layer = keras.Input(shape=input_shape, name=expert_name + '_input')
             x = Dense(intermediate_neurons, activation='tanh', input_shape=input_shape)(input_layer)
             output_layer = Dense(1, activation='tanh')(x) 
             model = keras.Model(input_layer, output_layer, name=expert_name)
-            experts.append(model)
-        return np.array(experts)
+            experts[class_name] = model
+        return experts
